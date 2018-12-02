5aa51e9241394161a3ba9d38432297bd58324dec
pointmoon <dendy1996@yandex.ru>
2018-12-02 12:32:52 +0300

    Переработан ввод данных на страницу

--- TestSelenium/Form1.cs
+++ TestSelenium/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using OpenQA.Selenium;
 using System.IO;
 
+
 namespace TestSelenium
 {
     public partial class Form1 : Form
@@ -17,6 +18,7 @@ namespace TestSelenium
         private IWebDriver Browser;
         private StreamWriter fout = new StreamWriter("instagram.dat");
         private Random random_str = new Random(Environment.TickCount);
+        private string UserAgent = "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36";
 
         public Form1()
         {
@@ -30,77 +32,66 @@ namespace TestSelenium
 
             OpenQA.Selenium.Firefox.FirefoxProfile profile = new OpenQA.Selenium.Firefox.FirefoxProfile();
             OpenQA.Selenium.Firefox.FirefoxOptions option = new OpenQA.Selenium.Firefox.FirefoxOptions();
-            profile.SetPreference("general.useragent.override", "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36");
+            profile.SetPreference("general.useragent.override", UserAgent);
             option.Profile = profile;
             Browser = new OpenQA.Selenium.Firefox.FirefoxDriver(option);
 
             Browser.Manage().Window.Maximize();
             Browser.Navigate().GoToUrl("https://www.instagram.com/");
             Browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            //эл.адрес
+
+            //----------------эл.адрес----------------
             IWebElement email = Browser.FindElement(By.Name("emailOrPhone"));
             string str_email_1 = RandomString(7);
             string str_email_2 = "@" + "yandex.com";
             string str_email = str_email_1 + str_email_2;
-            email.SendKeys(str_email_1);
+            SendToPage(email, str_email_1);
+            SendToPage(email, str_email_2);
             //антиБОТ
-            System.Threading.Thread.Sleep(5000);
-            email.SendKeys(str_email_2);
-            //антиБОТ
-            System.Threading.Thread.Sleep(5000);
+            System.Threading.Thread.Sleep(1000);
 
-            //имя и фамилия
+            //----------------имя и фамилия----------------
             IWebElement name = Browser.FindElement(By.Name("fullName"));
             string str_name_1 = RandomString(7);
             string str_name_2 = " ";
             string str_name_3 = RandomString(8);
             string str_name = str_name_1 + str_name_2 + str_name_3;
-            name.SendKeys(str_name_1);
+            SendToPage(name, str_name_1);
+            SendToPage(name, str_name_2);
+            SendToPage(name, str_name_3);
             //антиБОТ
-            System.Threading.Thread.Sleep(5000);
-            name.SendKeys(str_name_2);
-            //антиБОТ
-            System.Threading.Thread.Sleep(5000);
-            name.SendKeys(str_name_3);
-            //антиБОТ
-            System.Threading.Thread.Sleep(5000);
+            System.Threading.Thread.Sleep(1000);
 
-            //имя пользователя
+            //----------------имя пользователя----------------
             IWebElement username = Browser.FindElement(By.Name("username"));
             string str_username_1 = RandomString(5);
             string str_username_2 = RandomString(5);
             string str_username = str_username_1 + str_username_2;
-            username.SendKeys(str_username_1);
+            SendToPage(username, str_username_1);
+            SendToPage(username, str_username_2);
             //антиБОТ
-            System.Threading.Thread.Sleep(5000);
-            username.SendKeys(str_username_2);
-            //антиБОТ
-            System.Threading.Thread.Sleep(5000);
+            System.Threading.Thread.Sleep(1000);
 
-            //пароль
+            //----------------пароль----------------
             IWebElement password = Browser.FindElement(By.Name("password"));
             string str_password_1 = RandomString(2);
             string str_password_2 = RandomString(4);
             string str_password_3 = RandomString(4);
             string str_password = str_password_1 + str_password_2 + str_password_3;
-            password.SendKeys(str_password_1);
+            SendToPage(password, str_password_1);
+            SendToPage(password, str_password_2);
+            SendToPage(password, str_password_3);
             //антиБОТ
             System.Threading.Thread.Sleep(5000);
-            password.SendKeys(str_password_2);
-            //антиБОТ
-            System.Threading.Thread.Sleep(5000);
-            password.SendKeys(str_password_3);
-            //антиБОТ
-            System.Threading.Thread.Sleep(10000);
 
             password.SendKeys(OpenQA.Selenium.Keys.Enter);
 
-            //вывод в программу
+            //----------------вывод в программу----------------
             textBox1.AppendText("Эл. адрес: " + str_email + "\n");
             textBox1.AppendText("Имя и Файмилия: " + str_name + "\n");
             textBox1.AppendText("Имя пользователя: " + str_username + "\n");
             textBox1.AppendText("Пароль: " + str_password + "\n");
-            //вывод в файл
+            //----------------вывод в файл----------------
             fout.Write("Эл. адрес: " + str_email + "\n");
             fout.Write("Имя и Файмилия: " + str_name + "\n");
             fout.Write("Имя пользователя: " + str_username + "\n");
@@ -140,23 +131,35 @@ namespace TestSelenium
             Browser.Quit();
         }
 
-        //функция для генерации рандомных строк указанной длинны
+        //функция для генерации рандомного символа
         public string RandomString(int length)
         {
             string chars = "_1234567890abcdefghijklmnopqrstuvwxyz";
             StringBuilder builder = new StringBuilder(length);
-            for(int i=0; i<length; ++i)
+            for (int i = 0; i < length; ++i)
             {
                 builder.Append(chars[random_str.Next(chars.Length)]);
             }
             return builder.ToString();
         }
 
+        //процедура для заполнения поля ввода на странице
+        public void SendToPage(IWebElement elem, string str)
+        {
+            for(int i = 0; i< str.Length; i++)
+            {
+                elem.SendKeys(str[i].ToString());
+                System.Threading.Thread.Sleep(631);
+            }
+        }
+
+        // обработчик кнопки "Завершение работы"
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
             fout.Close();
         }
 
+        //обработчик кнопки "Повторить"
         private void button3_Click(object sender, EventArgs e)
         {
             Application.Restart();
